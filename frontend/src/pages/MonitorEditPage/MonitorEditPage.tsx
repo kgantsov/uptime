7789ce1822783bd5f786239926878b091f6b1574
Kostiantyn Hantsov <k.gantsov@gmail.com>
2022-12-19 18:52:45 +0100

    Add form validation for add/edit monitors forms using react-hook-form

--- frontend/src/pages/MonitorEditPage/MonitorEditPage.tsx
+++ frontend/src/pages/MonitorEditPage/MonitorEditPage.tsx
@@ -3,29 +3,37 @@ import { useParams, useNavigate } from 'react-router-dom';
 import { API } from '../../API';
 import { Button } from "@tremor/react";
 import Async from 'react-select/async';
-import { OptionsOrGroups, GroupBase } from 'react-select';
+import { useForm, SubmitHandler } from "react-hook-form";
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from "yup";
 
 import styles from './MonitorPage.module.css';
+
+type Inputs = {
+  name: string,
+  url: string,
+  check_interval: number,
+  accepted_status_code: number,
+  timeout: number,
+};
+
+const schema = yup.object({
+  name: yup.string().required(),
+  url: yup.string().required(),
+  check_interval: yup.number().integer().min(5).max(3600).required(),
+  accepted_status_code: yup.number().min(100).max(600).integer().required(),
+  timeout: yup.number().integer().min(0).max(120).required(),
+}).required();
+
 
 export function MonitorEditPage() {
   let navigate = useNavigate();
   const { monitorId } = useParams();
-
-  const [values, setValues] = useState({
-    name: '',
-    url: '',
-    check_interval: '',
-    timeout: '',
-    accepted_status_code: '',
+  const { register, setValue, handleSubmit, watch, formState: { errors } } = useForm<Inputs>({
+    resolver: yupResolver(schema)
   });
-  const [notifications, setNotifications] = useState<any[]>([]);
 
-  const handleChange = (e: { target: { name: any; value: any; }; }) => {
-    setValues((oldValues) => ({
-      ...oldValues,
-      [e.target.name]: e.target.value,
-    }));
-  };
+  const [notifications, setNotifications] = useState<any[]>([]);
 
   const promiseOptions = (inputValue: string): Promise<any> => {
     return new Promise((resolve) => {
@@ -42,136 +50,115 @@ export function MonitorEditPage() {
 
   useEffect(() => {
     API.fetch('GET', `/API/v1/services/${monitorId}`).then((data) => {
-      setValues(data)
+      setValue('name', data.name);
+      setValue('url', data.url);
+      setValue('check_interval', data.check_interval);
+      setValue('accepted_status_code', data.accepted_status_code);
+      setValue('timeout', data.timeout);
       setNotifications(data.notifications.map((notification: { name: any; }) => {
         return { value: notification.name, label: notification.name, ...notification };
       }))
     })
   }, [monitorId])
 
-  const handleSubmit = (e?: { preventDefault: () => void; }) => {
-    if (e !== undefined) {
-      e.preventDefault();
-    }
+  const onSubmit: SubmitHandler<Inputs> = data => {
 
     API.fetch('PATCH', `/API/v1/services/${monitorId}`, null, {
-      name: values.name,
-      url: values.url,
-      check_interval: Number.parseInt(values.check_interval),
-      timeout: Number.parseInt(values.timeout),
-      accepted_status_code: Number.parseInt(values.accepted_status_code),
+      name: data.name,
+      url: data.url,
+      check_interval: data.check_interval,
+      timeout: data.timeout,
+      accepted_status_code: data.accepted_status_code,
       notifications: notifications,
     }).then((data) => {
       navigate(`/monitors/${monitorId}`);
     });
   };
 
-    return (
-      <>
-        <div className='block'>
-          <h1>Edit Monitor</h1>
-          <form method="post" onSubmit={handleSubmit}>
-            <div className="form-element">
-              <label htmlFor="name">Name</label>
-              <input
-                className=""
-                id="name"
-                name="name"
-                type="text"
-                value={values.name}
-                onChange={handleChange}
-                required
-              />
-            </div>
+  return (
+    <>
+      <div className='block'>
+        <h1>Edit Monitor</h1>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className={(errors.name) ? "form-element error" : "form-element"}>
+            <label htmlFor="name">Name</label>
+            <input
+              type="text"
+              {...register("name", { required: 'Name is required' })}
+            />
+            <div className="error-message">{errors.name?.message}</div>
+          </div>
 
-            <div className="form-element">
-              <label htmlFor="url">URL</label>
-              <input
-                className=""
-                id="url"
-                name="url"
-                type="text"
-                value={values.url}
-                onChange={handleChange}
-                required
-              />
-            </div>
+          <div className={(errors.url) ? "form-element error" : "form-element"}>
+            <label htmlFor="url">URL</label>
+            <input
+              type="text"
+              {...register("url", { required: 'URL is required' })}
+            />
+            <div className="error-message">{errors.url?.message}</div>
+          </div>
 
-            <div className="form-element">
-              <label htmlFor="check_interval">check interval</label>
-              <input
-                className=""
-                id="check_interval"
-                name="check_interval"
-                type="number"
-                value={values.check_interval}
-                onChange={handleChange}
-                required
-              />
-            </div>
+          <div className={(errors.check_interval) ? "form-element error" : "form-element"}>
+            <label htmlFor="check_interval">check interval</label>
+            <input
+              type="number"
+              {...register("check_interval", { required: 'Check interval is required' })}
+            />
+            <div className="error-message">{errors.check_interval?.message}</div>
+          </div>
 
-            <div className="form-element">
-              <label htmlFor="timeout">Timeout</label>
-              <input
-                className=""
-                id="timeout"
-                name="timeout"
-                type="number"
-                value={values.timeout}
-                onChange={handleChange}
-                required
-              />
-            </div>
+          <div className={(errors.timeout) ? "form-element error" : "form-element"}>
+            <label htmlFor="timeout">Timeout</label>
+            <input
+              type="number"
+              {...register("timeout", { required: 'Timeout interval is required' })}
+            />
+            <div className="error-message">{errors.timeout?.message}</div>
+          </div>
 
-            <div className="form-element">
-              <label htmlFor="accepted_status_code">Accepted status code</label>
-              <input
-                className=""
-                id="accepted_status_code"
-                name="accepted_status_code"
-                type="number"
-                min={100}
-                max={600}
-                value={values.accepted_status_code}
-                onChange={handleChange}
-                required
-              />
-            </div>
+          <div className={(errors.accepted_status_code) ? "form-element error" : "form-element"}>
+            <label htmlFor="accepted_status_code">Accepted status code</label>
+            <input
+              type="number"
+              {...register("accepted_status_code", { required: 'Accepted status code is required' })}
+            />
+            <div className="error-message">{errors.accepted_status_code?.message}</div>
+          </div>
 
-            <div className="form-element">
-              <label htmlFor="callback_chat_id">Notifications</label>
-              <Async
-                className="react-select-container"
-                classNamePrefix="react-select"
-                cacheOptions
-                defaultOptions
-                isMulti={true}
-                loadOptions={promiseOptions}
-                placeholder='Select'
-                name="narratives"
-                value={notifications}
-                onChange={(option: readonly any[]) => {
-                  setNotifications([...option])
-                }}
-              />
-            </div>
+          <div className="form-element">
+            <label htmlFor="callback_chat_id">Notifications</label>
+            <Async
+              className="react-select-container"
+              classNamePrefix="react-select"
+              cacheOptions
+              defaultOptions
+              isMulti={true}
+              loadOptions={promiseOptions}
+              placeholder='Select'
+              name="narratives"
+              value={notifications}
+              onChange={(option: readonly any[]) => {
+                setNotifications([...option])
+              }}
+            />
+          </div>
 
-            <div className="form-element">
-              <div className="submit-wrapper">
-                <Button
-                    text="Save"
-                    icon={undefined}
-                    iconPosition="left"
-                    size="sm"
-                    color="green"
-                    importance="primary"
-                    handleClick={handleSubmit}
-                    marginTop="mt-0"
-                />
-              </div>
+          <div className="form-element">
+            <div className="submit-wrapper">
+              <Button
+                text="Save"
+                icon={undefined}
+                iconPosition="left"
+                size="sm"
+                color="green"
+                importance="primary"
+                handleClick={handleSubmit(onSubmit)}
+                marginTop="mt-0"
+            />
             </div>
-          </form>
-        </div>
-      </>
-    )
-  }
+          </div>
+        </form>
+      </div>
+    </>
+  )
+}
--- frontend/src/pages/MonitorNewPage/MonitorNewPage.tsx
+++ frontend/src/pages/MonitorNewPage/MonitorNewPage.tsx
@@ -4,27 +4,35 @@ import { API } from '../../API';
 import Async from 'react-select/async';
 import { Button } from "@tremor/react";
 import { FaPlus } from 'react-icons/fa';
+import { useForm, SubmitHandler } from "react-hook-form";
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from "yup";
 
 import styles from './MonitorNewPage.module.css';
 
+type Inputs = {
+  name: string,
+  url: string,
+  check_interval: number,
+  accepted_status_code: number,
+  timeout: number,
+};
+
+const schema = yup.object({
+  name: yup.string().required(),
+  url: yup.string().required(),
+  check_interval: yup.number().integer().min(5).max(3600).required(),
+  accepted_status_code: yup.number().min(100).max(600).integer().required(),
+  timeout: yup.number().integer().min(0).max(120).required(),
+}).required();
+
 export function MonitorNewPage() {
   let navigate = useNavigate();
-
-  const [values, setValues] = useState({
-    name: '',
-    url: '',
-    check_interval: '',
-    timeout: '',
-    accepted_status_code: '',
+  const { register, handleSubmit, watch, formState: { errors } } = useForm<Inputs>({
+    resolver: yupResolver(schema)
   });
-  const [notifications, setNotifications] = useState<any[]>([]);
 
-  const handleChange = (e: { target: { name: any; value: any; }; }) => {
-    setValues((oldValues) => ({
-      ...oldValues,
-      [e.target.name]: e.target.value,
-    }));
-  };
+  const [notifications, setNotifications] = useState<any[]>([]);
 
   const promiseOptions = (inputValue: string): Promise<any> => {
     return new Promise((resolve) => {
@@ -39,126 +47,105 @@ export function MonitorNewPage() {
     });
   }
 
-  const handleSubmit = (e?: { preventDefault: () => void; }) => {
-    if (e !== undefined) {
-      e.preventDefault();
-    }
-
+  const onSubmit: SubmitHandler<Inputs> = data => {
     API.fetch('POST', `/API/v1/services`, null, {
-      name: values.name,
-      url: values.url,
-      check_interval: Number.parseInt(values.check_interval),
-      accepted_status_code: Number.parseInt(values.accepted_status_code),
-      timeout: Number.parseInt(values.timeout),
+      name: data.name,
+      url: data.url,
+      check_interval: data.check_interval,
+      accepted_status_code: data.accepted_status_code,
+      timeout: data.timeout,
       notifications: notifications,
       enabled: true,
     }).then((data) => {
       navigate(`/monitors/${data['id']}`);
     });
-  };
-
-    return (
-      <>
-        <div className='block'>
-          <h1>Add New Monitor</h1>
-          <form method="post" onSubmit={handleSubmit}>
-            <div className="form-element">
-              <label htmlFor="name">Name</label>
-              <input
-                className=""
-                id="name"
-                name="name"
-                type="text"
-                onChange={handleChange}
-                required
-              />
-            </div>
-
-            <div className="form-element">
-              <label htmlFor="url">URL</label>
-              <input
-                className=""
-                id="url"
-                name="url"
-                type="text"
-                onChange={handleChange}
-                required
-              />
-            </div>
-
-            <div className="form-element">
-              <label htmlFor="check_interval">check interval</label>
-              <input
-                className=""
-                id="check_interval"
-                name="check_interval"
-                type="number"
-                onChange={handleChange}
-                required
-              />
-            </div>
-
-            <div className="form-element">
-              <label htmlFor="timeout">Timeout</label>
-              <input
-                className=""
-                id="timeout"
-                name="timeout"
-                type="number"
-                onChange={handleChange}
-                required
-              />
-            </div>
-
-            <div className="form-element">
-              <label htmlFor="accepted_status_code">Accepted status code</label>
-              <input
-                className=""
-                id="accepted_status_code"
-                name="accepted_status_code"
-                type="number"
-                min={100}
-                max={600}
-                value={values.accepted_status_code}
-                onChange={handleChange}
-                required
-              />
-            </div>
-
-            <div className="form-element">
-              <label htmlFor="callback_chat_id">Notifications</label>
-              <Async
-                className="react-select-container"
-                classNamePrefix="react-select"
-                cacheOptions
-                defaultOptions
-                isMulti={true}
-                loadOptions={promiseOptions}
-                placeholder='Select'
-                name="narratives"
-                value={notifications}
-                onChange={(option: readonly any[]) => {
-                  setNotifications([...option])
-                }}
-              />
-            </div>
-
-            <div className="form-element">
-              <div className="submit-wrapper">
-                <Button
-                    text="Create"
-                    icon={FaPlus}
-                    iconPosition="left"
-                    size="sm"
-                    color="green"
-                    importance="primary"
-                    handleClick={handleSubmit}
-                    marginTop="mt-0"
-                />
-              </div>
-            </div>
-          </form>
-        </div>
-      </>
-    )
   }
+
+  console.log('=====>', errors)
+
+  return (
+    <>
+      <div className='block'>
+        <h1>Add New Monitor</h1>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className={(errors.name) ? "form-element error" : "form-element"}>
+            <label htmlFor="name">Name</label>
+            <input
+              type="text"
+              {...register("name", { required: 'Name is required' })}
+            />
+            <div className="error-message">{errors.name?.message}</div>
+          </div>
+
+          <div className={(errors.url) ? "form-element error" : "form-element"}>
+            <label htmlFor="url">URL</label>
+            <input
+              type="text"
+              {...register("url", { required: 'URL is required' })}
+            />
+            <div className="error-message">{errors.url?.message}</div>
+          </div>
+
+          <div className={(errors.check_interval) ? "form-element error" : "form-element"}>
+            <label htmlFor="check_interval">check interval</label>
+            <input
+              type="number"
+              {...register("check_interval", { required: 'Check interval is required' })}
+            />
+            <div className="error-message">{errors.check_interval?.message}</div>
+          </div>
+
+          <div className={(errors.timeout) ? "form-element error" : "form-element"}>
+            <label htmlFor="timeout">Timeout</label>
+            <input
+              type="number"
+              {...register("timeout", { required: 'Timeout interval is required' })}
+            />
+            <div className="error-message">{errors.timeout?.message}</div>
+          </div>
+
+          <div className={(errors.accepted_status_code) ? "form-element error" : "form-element"}>
+            <label htmlFor="accepted_status_code">Accepted status code</label>
+            <input
+              type="number"
+              {...register("accepted_status_code", { required: 'Accepted status code is required' })}
+            />
+            <div className="error-message">{errors.accepted_status_code?.message}</div>
+          </div>
+
+          <div className="form-element">
+            <label htmlFor="callback_chat_id">Notifications</label>
+            <Async
+              className="react-select-container"
+              classNamePrefix="react-select"
+              cacheOptions
+              defaultOptions
+              isMulti={true}
+              loadOptions={promiseOptions}
+              placeholder='Select'
+              value={notifications}
+              onChange={(option: readonly any[]) => {
+                setNotifications([...option])
+              }}
+            />
+          </div>
+
+          <div className="form-element">
+            <div className="submit-wrapper">
+              <Button
+                  text="Create"
+                  icon={FaPlus}
+                  iconPosition="left"
+                  size="sm"
+                  color="green"
+                  importance="primary"
+                  handleClick={handleSubmit(onSubmit)}
+                  marginTop="mt-0"
+              />
+            </div>
+          </div>
+        </form>
+      </div>
+    </>
+  )
+}
