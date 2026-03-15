522ee662aeb5200e5335a07f0c82ae955df96cf6
Kostiantyn Hantsov <k.gantsov@gmail.com>
2026-03-15 17:48:43 +0100

    Make stats card to take 100% width on mobile devices

--- frontend/src/pages/MonitorPage/MonitorCards.tsx
+++ frontend/src/pages/MonitorPage/MonitorCards.tsx
@@ -1,95 +1,115 @@
-import { useState, useEffect, SetStateAction } from 'react';
-import { HeartbeatStats, STATUS_COLORS_MAP } from '../../types/heartbeats';
-import { Card, Metric, Title, Text, Flex, CategoryBar, DonutChart, Divider } from "@tremor/react";
+import { useState, useEffect, SetStateAction } from "react";
+import { HeartbeatStats, STATUS_COLORS_MAP } from "../../types/heartbeats";
+import {
+  Card,
+  Metric,
+  Title,
+  Text,
+  Flex,
+  CategoryBar,
+  DonutChart,
+  Divider,
+} from "@tremor/react";
 
-import styles from './MonitorPage.module.css';
-import { API } from '../../API';
-
+import styles from "./MonitorPage.module.css";
+import { API } from "../../API";
 
 interface Props {
-    monitorId: string | undefined;
+  monitorId: string | undefined;
 }
 
 interface StatsData {
-    UP: HeartbeatStats;
-    DOWN: HeartbeatStats;
-    TIMEOUT: HeartbeatStats;
-    FAILED: HeartbeatStats;
-    UNKNOWN: HeartbeatStats;
+  UP: HeartbeatStats;
+  DOWN: HeartbeatStats;
+  TIMEOUT: HeartbeatStats;
+  FAILED: HeartbeatStats;
+  UNKNOWN: HeartbeatStats;
 }
 
-
 export function MonitorCards({ monitorId }: Props): JSX.Element {
+  const [statsDay, setStatsDay] = useState<StatsData | undefined>(undefined);
+  const [statsWeek, setStatsWeek] = useState<StatsData | undefined>(undefined);
+  const [statsMonth, setStatsMonth] = useState<StatsData | undefined>(
+    undefined,
+  );
 
-    const [statsDay, setStatsDay] = useState<StatsData|undefined>(undefined);
-    const [statsWeek, setStatsWeek] = useState<StatsData|undefined>(undefined);
-    const [statsMonth, setStatsMonth] = useState<StatsData|undefined>(undefined);
+  async function fetchStatsData(
+    days: number,
+    setDataFunc: {
+      (value: SetStateAction<StatsData | undefined>): void;
+      (value: SetStateAction<StatsData | undefined>): void;
+      (arg0: any): void;
+    },
+  ) {
+    try {
+      const resp = await API.fetch("GET", `/API/v1/heartbeats/stats/${days}`);
+      const data = await resp.json();
+      setDataFunc(
+        data
+          .filter((x: HeartbeatStats) => x.service_id === Number(monitorId))
+          .reduce((a: any, v: HeartbeatStats) => ({ ...a, [v.status]: v }), {}),
+      );
+    } catch (e) {
+      console.log(e);
+    }
+  }
 
-    async function fetchStatsData(days: number, setDataFunc: { (value: SetStateAction<StatsData | undefined>): void; (value: SetStateAction<StatsData | undefined>): void; (arg0: any): void; }) {
-        try {
-          const resp = await API.fetch('GET', `/API/v1/heartbeats/stats/${days}`);
-          const data = await resp.json();
-          setDataFunc(
-            data
-              .filter((x: HeartbeatStats) => x.service_id === Number(monitorId))
-              .reduce(
-                (a: any, v: HeartbeatStats) => ({ ...a, [v.status]: v}),
-                {}
-              )
-          )
-        } catch(e) {
-            console.log(e);
-        }
-      }
+  useEffect(() => {
+    fetchStatsData(1, setStatsDay);
+    fetchStatsData(7, setStatsWeek);
+    fetchStatsData(30, setStatsMonth);
 
-
-    useEffect(() => {
+    const interval = setInterval(() => {
       fetchStatsData(1, setStatsDay);
       fetchStatsData(7, setStatsWeek);
       fetchStatsData(30, setStatsMonth);
+    }, 5000);
+    return () => clearInterval(interval);
+  }, [monitorId]);
 
-      const interval = setInterval(() => {
-        fetchStatsData(1, setStatsDay);
-        fetchStatsData(7, setStatsWeek);
-        fetchStatsData(30, setStatsMonth);
-      }, 5000);
-      return () => clearInterval(interval);
-    }, [monitorId]);
-
-    if (!statsDay || !statsWeek || !statsMonth) {
-      return <></>
-    }
-
-    const calcPercentage = (data: StatsData): number => {
-        const success = data?.UP?.counter | 0;
-        const total = Object.values(data).reduce((a: number, v: HeartbeatStats) => a + v.counter, 0);
-        
-        return (success > 0) ? 100 / total * success : 0
-    }
-
-    return (
-        <div className='cards'>
-            <Card maxWidth="max-w-xs" decoration="top" decorationColor="orange">
-                <Metric>24 hours</Metric>
-                <Text>Avg. Response</Text>
-                <Title>{Math.round(statsDay['UP']?.average_response_time || 0)} ms</Title>
-                <Text>Uptime</Text>
-                <Title>{calcPercentage(statsDay).toFixed(2)}%</Title>
-            </Card>
-            <Card maxWidth="max-w-xs" decoration="top" decorationColor="orange">
-                <Metric>7 days</Metric>
-                <Text>Avg. Response</Text>
-                <Title>{Math.round(statsWeek['UP']?.average_response_time || 0)} ms</Title>
-                <Text>Uptime</Text>
-                <Title>{calcPercentage(statsWeek).toFixed(2)}%</Title>
-            </Card>
-            <Card maxWidth="max-w-xs" decoration="top" decorationColor="orange">
-                <Metric>30 days</Metric>
-                <Text>Avg. Response</Text>
-                <Title>{Math.round(statsMonth['UP']?.average_response_time || 0)} ms</Title>
-                <Text>Uptime</Text>
-                <Title>{calcPercentage(statsMonth).toFixed(2)}%</Title>
-            </Card>
-        </div>
-    )
+  if (!statsDay || !statsWeek || !statsMonth) {
+    return <></>;
   }
+
+  const calcPercentage = (data: StatsData): number => {
+    const success = data?.UP?.counter | 0;
+    const total = Object.values(data).reduce(
+      (a: number, v: HeartbeatStats) => a + v.counter,
+      0,
+    );
+
+    return success > 0 ? (100 / total) * success : 0;
+  };
+
+  return (
+    <div className="cards">
+      <Card decoration="top" decorationColor="orange">
+        <Metric>24 hours</Metric>
+        <Text>Avg. Response</Text>
+        <Title>
+          {Math.round(statsDay["UP"]?.average_response_time || 0)} ms
+        </Title>
+        <Text>Uptime</Text>
+        <Title>{calcPercentage(statsDay).toFixed(2)}%</Title>
+      </Card>
+      <Card decoration="top" decorationColor="orange">
+        <Metric>7 days</Metric>
+        <Text>Avg. Response</Text>
+        <Title>
+          {Math.round(statsWeek["UP"]?.average_response_time || 0)} ms
+        </Title>
+        <Text>Uptime</Text>
+        <Title>{calcPercentage(statsWeek).toFixed(2)}%</Title>
+      </Card>
+      <Card decoration="top" decorationColor="orange">
+        <Metric>30 days</Metric>
+        <Text>Avg. Response</Text>
+        <Title>
+          {Math.round(statsMonth["UP"]?.average_response_time || 0)} ms
+        </Title>
+        <Text>Uptime</Text>
+        <Title>{calcPercentage(statsMonth).toFixed(2)}%</Title>
+      </Card>
+    </div>
+  );
+}
